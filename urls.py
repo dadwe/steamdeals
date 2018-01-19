66b98480aee3eefe421ebcedeaccad97ebf2f06d
dadwe <sstevenhuang98@gmail.com>
2018-01-19 05:11:44 +0000

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from . import views
+from django.contrib.auth.views import login
+from django.contrib.auth import views as auth_views
+
+urlpatterns= [
+    url('^$', views.other_index, name='other_index'),
+    url('login/$', login, {'template_name': 'listapp/login.html'}),
+    url('home/$', views.home_index, name='home_index'),
+    url('logout/$', views.logout_index, name="logout_index"),
+    url('steamsales/$', views.steamsales_index, name='steamsales_index'),
+    url('humblesales/$', views.humblesales_index, name='humblesales_index'),
+    ]
---
+++ views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from listapp.models import Steam, Steam_g2a, Humble, HumbleG2a
+
+
+def other_index(request):
+    return render(request, "listapp/landing.html")
+
+
+@login_required(login_url="/login")
+def home_index(request):
+    return render(request, "listapp/home.html")
+
+
+def login_index(request):
+    return render(request, "listapp/login.html")
+
+def logout_index(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('/login')
+
+@login_required(login_url="/login")
+def steamsales_index(request):
+    sales = Steam.objects.all()
+    g2a_steam = Steam_g2a.objects.all()
+    return render(request, "listapp/steamsales.html", {'sales': sales,
+                                                       'g2a_steam': g2a_steam})
+
+@login_required(login_url="/login")
+def humblesales_index(request):
+    sales = Humble.objects.all()
+    g2a_humble = HumbleG2a.objects.all()
+    return render(request, "listapp/humblesales.html", {'sales': sales,
+                                                       'g2a_humble': g2a_humble})
